058b836fc8f7c529111facd732a51b4d16c686be
suneelprakashriktam <suneel@riktamtech.com>
2013-04-16 18:15:17 +0530

    Static Screens Implementation For Printing

---
+++ Project/src/com/riktamtech/picapoco/ui/PrintScreenTwoActivity.java
@@ -0,0 +1,15 @@
+package com.riktamtech.picapoco.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.riktamtech.picapoco.R;
+
+public class PrintScreenTwoActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_print_screen_two);
+	}
+}
